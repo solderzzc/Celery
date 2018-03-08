5d75f452d1b6c4e3d893eaaee0c20343894c0a51
Simba Zhang <solderzzc@gmail.com>
2018-03-08 15:07:18 -0800

    More test code.

--- test.py
+++ test.py
@@ -9,6 +9,19 @@ resp = requests.post(url, data=json.dumps(args))
 reply = resp.json()
 print reply
 
+url = '{}/queues/length'.format(api_root)
+print(url)
+resp = requests.get(url)
+print 'Queues length is {}'.format(resp.json())
+
+## Get list
+url = '{}/tasks'.format(api_root)
+print(url)
+payload = {'state': 'STARTED','taskname':'deepeye.tasks.detect'}
+resp = requests.get(url,params=payload)
+print 'task running is {}'.format(resp.json())
+
+
 print 'Test block call'
 url = '{}/apply/deepeye.tasks.detect'.format(task_api)
 print(url)
